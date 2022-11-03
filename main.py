7abebeead9c0c690e1bd7d66096b8a3c9b5b9fe3
bebitzko <59434201+bebitzko@users.noreply.github.com>
2022-11-03 14:21:04 -0500

    Remove infinite-play loop
    The flow is a bit weird with the infinite loop. Instead, I'm planning to, at the end of fixing the game, challenge the students to suggest a way to make the game loop infinitely.

--- main.py
+++ main.py
@@ -2,33 +2,31 @@
 
 import random
 
-while True:
-
-    keepAsking == True
-    while keepAsking:
-        num = input(Type a number for an upper bound: )
-        if num.isdigit():
-            print("Let's play!")
-            num = int(num)
-            # something about the loop condition
-        else:
-            print("Invalid input. Try again.")
-
-    secret = random.randint(1, num)
-      guess = None
-    counter = 1
-
-    while guess != secret:
-        guess = input("Type a number between 1 and " str(num) ": ")
-        if guess.isdigit():
-            guess = int(guess)
-        if False: # can't always be wrong
-            print("You got it!")
-        else:
-            print("Try again.")
-            # what else?
-
-    if count == 1
-        print("It took you", count, "guess!")
+keepAsking == True
+while keepAsking:
+    num = input(Type a number for an upper bound: )
+    if num.isdigit():
+        print("Let's play!")
+        num = int(num)
+        # something about the loop condition
     else:
-        print("It took you", count, "guesses!")
+        print("Invalid input. Try again.")
+
+secret = random.randint(1, num)
+  guess = None
+counter = 1
+
+while guess != secret:
+    guess = input("Type a number between 1 and " str(num) ": ")
+    if guess.isdigit():
+        guess = int(guess)
+    if False: # can't always be wrong
+        print("You got it!")
+    else:
+        print("Try again.")
+        # what else?
+
+if count == 1
+    print("It took you", count, "guess!")
+else:
+    print("It took you", count, "guesses!")
