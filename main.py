d045fdb5cfb358a75e55ede8b50f66a01bd36642
wcc-at-work <116679187+wcc-at-work@users.noreply.github.com>
2022-11-04 01:06:18 +0000

    Fixed errors.

--- main.py
+++ main.py
@@ -2,22 +2,22 @@
 
 import random
 
-keepAsking == True
+keepAsking = True
 while keepAsking:
-    num = input(Type a number for an upper bound: )
+    num = input("Type a number for an upper bound: ")
     if num.isdigit():
         print("Let's play!")
         num = int(num)
-        # something about the loop condition
+        keepAsking = False
     else:
         print("Invalid input. Try again.")
 
 secret = random.randint(1, num)
-  guess = None
+guess = None
 counter = 1
 
 while guess != secret:
-    guess = input("Type a number between 1 and " str(num) ": ")
+    guess = input("Type a number between 1 and " + str(num) + ": ")
     if guess.isdigit():
         guess = int(guess)
     if False: # can't always be wrong
@@ -26,7 +26,7 @@ while guess != secret:
         print("Try again.")
         # what else?
 
-if count == 1
+if count == 1:
     print("It took you", count, "guess!")
 else:
     print("It took you", count, "guesses!")
